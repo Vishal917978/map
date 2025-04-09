a8e52c84183e415b1d25bf75c082a3a169c19a68
Vishal917978 <vishalsharma917978@gmail.com>
2025-04-09 13:38:11 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,12 @@
+let arr=["vishal","sharma"];
+let new1=arr.map((values)=>values.toUpperCase());
+console.log(new1);
+
+
+let arr1=["hey","bye","hello"];
+let n=arr.map((val)=>val+"!")
+console.log(n);
+
+let name=["vishal","pratibha"];
+let n2=name.map((val)=>val+" sharma");
+console.log(n2);
